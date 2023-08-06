7cb29e97859f0c9a96475cf91d41197db351c196
MrRuslanYT <pandacorpglobal@gmail.com>
2023-08-06 15:00:46 +0300

    added rxjava3 to build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,9 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:okhttp:4.11.0")
+
+    // RxJava
+    implementation ("io.reactivex.rxjava3:rxjava:3.1.6")
+    implementation ("io.reactivex.rxjava3:rxandroid:3.0.2")
+
 }
